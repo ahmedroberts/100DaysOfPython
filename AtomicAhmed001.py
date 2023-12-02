e7c144564e2588451fccb2f21c4cdba8c3e08020
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2023-12-01 22:51:01 -0500

    coded up a band generator

--- AtomicAhmed001.py
+++ AtomicAhmed001.py
@@ -1,2 +1,14 @@
 # ApogeeAOR | Apogee Aor | The 9th RaiKage
-print("Welcome to Ahmed's Band Name Generator")
+# I have this on Replit https://replit.com/@ahmedroberts/Day001-Band-Name#main.py
+
+# 1. Create a greeting for your program.
+print("\n\n\nWelcome to Ahmed's Band Name Generator!\nHe is th 9th RaiKage! \n\n")
+
+# 2. Ask the user for the city they grew up in.
+band_city = input("What city did you grow up in? \n")
+
+# 3. Ask the user for the name of a pet.
+band_pet = input("What was the name of your first pet? \n")
+
+# 4. Combine their city and pet to show them their band name.
+print("Possible band name: " + band_city + " " + band_pet + "\n**********\n")
