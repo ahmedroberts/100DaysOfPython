9a9f42b6d4c000d463c9b8b8aef05fadfbde88f9
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-01-21 17:32:16 -0500

    Add zPEMDAS.py, Exercise001.py

---
+++ Exercise001.py
@@ -0,0 +1,5 @@
+myCalculation = (3 * 3 - 3 ** 2 + 4 /2)
+print(myCalculation)
+
+myCalculation = (8 ** 2 / 4 - 9)
+print(myCalculation)
---
+++ zPEMDAS.py
@@ -0,0 +1,2 @@
+myCalculation = (3 * 3 - 3 ** 2 + 4 /2)
+print(myCalculation)
