00b551b90109445cfe558aca8604ad09151f5a70
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-01-22 23:56:21 -0500

    Update Set Method

--- zPEMDAS.py
+++ zPEMDAS.py
@@ -1,2 +1,7 @@
 myCalculation = (3 * 3 - 3 ** 2 + 4 /2)
 print(myCalculation)
+
+
+myRandomList = [0,1,5,9,3,6,5,4,2,2,5,1,3,0,2,2,5,7,4,7,4,7,4,7,4,7]
+print("Set: ", set(myRandomList))
+print(len(set(myRandomList)))
