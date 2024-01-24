061a05c3ef97e9909214d7d9c6fd39e8986b274f
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-01-24 18:54:13 -0500

    Reorganize files and test assignments

---
+++ Lesson05_Day02.py
@@ -0,0 +1,9 @@
+two_digit_number = input()
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+# Add the two integers together
+sum_of_digits = (first_digit + second_digit)
+
+print(sum_of_digits)
--- My_004.py
+++ My_004.py
@@ -2,15 +2,15 @@
 print("\nRaikage will switch two inputs.")
 
 # There are two variables, a and b from input
-a = input("What is the first input?")
-b = input("What is the second input?")
+a = input("What is the first input? ")
+b = input("What is the second input? ")
 # 🚨 Don't change the code above ☝️
 ####################################
 # Write your code below this line 👇
 
 print("\na was entered as: " + a)
 print("b was entered as: " + b)
-print("~~ presto change o ~ switcheroo ~~\n")
+print("\n~~ presto change o ~ switcheroo ~~\n")
 
 a = str(a)
 b = str(b)
@@ -21,3 +21,11 @@ b = c
 # 🚨 Don't change the code below 👇
 print("a: " + a)
 print("b: " + b)
+
+print(" **** Switcheroo **** ")
+a, b, c, d, e = "King", "Ahmed", "Is", "9th", "Raikage"
+print(a, b, c, d, e)
+
+print("\na, b, c, d, e = d, c, a, e, b\n")
+a, b, c, d, e = d, c, a, e, b
+print(a, b, c, d, e)
