53c35426e268db225150a86afec764dc07f8a2d7
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-01-28 23:42:08 -0500

    Add Weeks Left Calculator

--- Lesson07_Day02.py
+++ Lesson07_Day02.py
@@ -14,3 +14,13 @@ isWinning = True
 my_f_string = f"your score is {score},  your height is {height}, you are winning {isWinning}"
 
 print(my_f_string)
+
+# Life in Weeks
+
+current_age = input("What is your current age?: ")
+
+years_left = 90 - int(current_age)
+
+weeks_left = int(years_left * 52)
+
+print(f"You have {weeks_left} weeks left. Enjoy!")
