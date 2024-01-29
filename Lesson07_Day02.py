9b2ed18273e388a34f6957b3afb43d8d1ac6a02a
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-01-28 23:08:15 -0500

    Add f-string in python

--- Lesson07_Day02.py
+++ Lesson07_Day02.py
@@ -5,3 +5,12 @@ Calculation_002 = 8//3
 print("Calc 001  = ", Calculation_001, "Calc 002 = ", Calculation_002)
 
 print(round(Calculation_001, 2))
+
+score = 0
+height = 1.8
+isWinning = True
+
+# f-string
+my_f_string = f"your score is {score},  your height is {height}, you are winning {isWinning}"
+
+print(my_f_string)
