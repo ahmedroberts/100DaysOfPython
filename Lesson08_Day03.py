9751dcd5e83813c513c5319bba2e8bed9507a792
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-02-04 20:59:00 -0500

    Update Rollecoaster Code

--- Lesson08_Day03.py
+++ Lesson08_Day03.py
@@ -1,3 +1,15 @@
+'''
 number = int(input("Please enter a number. "))
 
 print("This is an even number.") if (number % 2 == 0) else print("This is an odd number.")
+'''
+
+print("\n\tWelcome to the Adora Rollercoaster\n")
+height = int(input("What is your height in cm? : "))
+
+if height > 120:
+    print("Enjoy the ride!")
+else:
+    print("Sorry, maybe next time.")
+    
+input()
