85c4be3420d93491df55b263021f56b61e473ff5
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-02-05 23:54:37 -0500

    Update Rollercoaster Age Price

--- Lesson08_Day03.py
+++ Lesson08_Day03.py
@@ -9,6 +9,11 @@ height = int(input("What is your height in cm? : "))
 
 if height > 120:
     print("Enjoy the ride!")
+    age = int(input("What is your age? "))
+    if age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("Sorry, maybe next time.")
     
