c9b7782ec3f920ab92f044bcbc87921cd68dff60
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-08-07 20:05:00 -0700

    :clown_face: start new workflow :truck:

---
+++ sec04_day04/modulo.py
@@ -0,0 +1,10 @@
+# Even Number
+
+print('\nCheck Evens.\n')
+
+num_max = int(input('What number shall we stop at?: '))
+for x in range(num_max + 1):
+  if x % 2 == 0:
+    print(f'{x:>3} is even.')
+
+print('\nAll done.')
