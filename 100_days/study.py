379b1b480843691892f61ca86628a23511abdfd9
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-08-07 19:52:14 -0700

    :coffin: clean up the workspace :truck:

---
+++ 100_days/study.py
@@ -0,0 +1,19 @@
+# Ahmed O. Roberts
+# The Ninth RaiKage
+# July 1st, 2023
+
+name = input('What is your name?\n')
+print('Hi, %s.' % name)
+
+#1. Create a greeting for your program.
+print("Welcome to Ahmed's Band Name Generator!!")
+#2. Ask the user for the city that they grew up in.
+band_city = input("What city did you grow up in? \n ")
+#3. Ask the user for the name of a pet.
+band_pet = input("What was the name of your first pet? \n ")
+#4. Combine the name of their city and pet and show them their band name.
+print("Possible band name:\n", band_city, band_pet)
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
