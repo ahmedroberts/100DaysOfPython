6b2962bd55254f6270bdeee7f5a5729eacdef424
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-08-08 16:18:49 -0700

    quick update

--- sec04_day04/modulo.py
+++ sec04_day04/modulo.py
@@ -6,5 +6,7 @@ num_max = int(input('What number shall we stop at?: '))
 for x in range(num_max + 1):
   if x % 2 == 0:
     print(f'{x:>3} is even.')
+  else:
+    print(f'{x:>3} is odd.')
 
 print('\nAll done.')
